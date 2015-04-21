044fe99643905d90464fb3847142e90a9b272165
Michael Bestas <mikeioannina@gmail.com>
2015-04-22 02:15:28 +0300

    SystemUI: Add heads up tile
    xyyx: adapted for P and replaced drawbles
    
    Change-Id: Ied99d31630cd85bbb85f176987fbcf38d197d0cf
    Signed-off-by: xyyx <xyyx@mail.ru>
    
    [Stallix] Adapt for A13 QPR2 after b8d6c0a94834079f5598dc920a818fd4d67bf636.
    [Stallix] Adapt for A13 QPR3

--- packages/SystemUI/src/com/android/systemui/custom/CustomQSModule.kt
+++ packages/SystemUI/src/com/android/systemui/custom/CustomQSModule.kt
@@ -2,6 +2,7 @@ package com.android.systemui.custom
 
 import com.android.systemui.qs.tileimpl.QSTileImpl
 import com.android.systemui.qs.tiles.CaffeineTile
+import com.android.systemui.qs.tiles.HeadsUpTile
 import com.android.systemui.qs.tiles.PowerShareTile
 import com.android.systemui.qs.tiles.SoundTile
 import com.android.systemui.qs.tiles.SyncTile
@@ -19,6 +20,12 @@ interface CustomQSModule {
     @IntoMap
     @StringKey(CaffeineTile.TILE_SPEC)
     fun bindCaffeineTile(caffeineTile: CaffeineTile): QSTileImpl<*>
+
+    /** Inject HeadsUpTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(HeadsUpTile.TILE_SPEC)
+    fun bindHeadsUpTile(headsUpTile: HeadsUpTile): QSTileImpl<*>
 
     /** Inject PowerShareTile into tileMap in QSModule */
     @Binds
