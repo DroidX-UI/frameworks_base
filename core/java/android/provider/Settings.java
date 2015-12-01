bc387d48c9dbcdcfd48bc5ed440fe99ee8c2655e
rascarlo <rascarlo@gmail.com>
2015-12-01 19:18:53 +0100

    base: Volume Rocker Wake [1/2]
    Change-Id: I08595975642e5f5f3c71cc1ada43b1cd9819f952

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5527,6 +5527,12 @@ public final class Settings {
          */
         public static final String FINGERPRINT_WAKE_UNLOCK = "fingerprint_wake_unlock";
 
+
+        /** Volume rocker wake
+         * @hide
+         */
+        public static final String VOLUME_ROCKER_WAKE = "volume_rocker_wake";
+
         /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
@@ -5822,6 +5828,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(DESKTOP_MODE);
             PRIVATE_SETTINGS.add(FORCE_FULLSCREEN_CUTOUT_APPS);
             PRIVATE_SETTINGS.add(STATUS_BAR_CLOCK);
+            PRIVATE_SETTINGS.add(VOLUME_ROCKER_WAKE);
         }
 
         /**
