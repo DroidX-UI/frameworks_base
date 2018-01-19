c95fbd332042157836f919eaac7926adcef60c54
Pranav Vashi <neobuddy89@gmail.com>
2018-01-19 23:48:34 +0530

    TunerService: Prevent NPE with tunable
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
@@ -364,7 +364,9 @@ public class TunerServiceImpl extends TunerService {
         }
         String value = getValue(key);
         for (Tunable tunable : tunables) {
-            tunable.onTuningChanged(key, value);
+            if (tunable != null) {
+                tunable.onTuningChanged(key, value);
+            }
         }
     }
 
@@ -375,7 +377,9 @@ public class TunerServiceImpl extends TunerService {
             }
             String value = getValue(key);
             for (Tunable tunable : mTunableLookup.get(key)) {
-                tunable.onTuningChanged(key, value);
+                if (tunable != null) {
+                    tunable.onTuningChanged(key, value);
+                }
             }
         }
     }
