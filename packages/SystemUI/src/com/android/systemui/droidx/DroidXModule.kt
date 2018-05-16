74d8ff022122893145967f3195fedbdcb96482c1
Joey <joey@lineageos.org>
2018-05-16 17:16:06 +0200

    SystemUI: Add reading mode tile
    Author: Joey <joey@lineageos.org>
    Date:   Wed May 16 17:16:06 2018 +0200
    
        base: add reading mode tile
    
        Change-Id: I1c515cbeff0dd5a1fa0c00bc9390a66eb104c7f0
        Signed-off-by: Joey <joey@lineageos.org>
    
    Author: Michael Bestas <mkbestas@lineageos.org>
    Date:   Sat May 26 00:58:57 2018 +0300
    
        SystemUI: Fix reading mode tile accessibility strings
    
        Change-Id: I4f0af56e4947b037e1e2dda79132523d13458818
    
    Author: Paul Keith <javelinanddart@gmail.com>
    Date:   Wed Aug 8 18:59:25 2018 +0200
    
        SystemUI: Update reading mode tile for new API
    
        Change-Id: I9fbf1b4de30079c987b75e37e52e234fa2f86c5d
    
    Change-Id: I365ee54b2b2adc52d804a5fc1d35690ef5636071
    
    commit 3dce873061757be3ff97dd4d48f90f71f1e8e004
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Sat Apr 11 14:09:58 2020 +0100
    
        ReadingModeTile: Go away if wellbeing app is enabled
    
        Google's wellbeing app provides Wind Down feature along with
        a grayscale option and a tile for it. Don't show up Reading Mode
        tile to avoid having duplicate features.
    
        Change-Id: I4735b57253a787c098984f456ce4da40dbd17c94
    
    commit a3246ce92dd7d8b401a96bf1d2241c3b05e61efe
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Fri May 14 23:06:29 2021 +0100
    
        ReadingModeTile: Drop dependency on wellbeing app
    
        Google's wellbeing app used to provide Wind Down feature along with
        a grayscale option and a tile for it. It is no longer the case,
        so bring back our tile unconditionally.
    
        Change-Id: I5a29d4f8f86dfe0361064842da8c9a3b2ce33d47
    
    Change-Id: Iffc81d37452cc6989f19715ab8508c517a7fcd83
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- packages/SystemUI/src/com/android/systemui/droidx/DroidXModule.kt
+++ packages/SystemUI/src/com/android/systemui/droidx/DroidXModule.kt
@@ -31,6 +31,7 @@ import com.android.systemui.qs.tiles.VpnTile
 import com.android.systemui.qs.tiles.PowerShareTile
 import com.android.systemui.qs.tiles.WifiTile
 import com.android.systemui.qs.tiles.LiveDisplayTile
+import com.android.systemui.qs.tiles.ReadingModeTile
 
 import dagger.Binds
 import dagger.Module
@@ -123,4 +124,10 @@ interface DroidXModule {
     @IntoMap
     @StringKey(LiveDisplayTile.TILE_SPEC)
     fun bindLiveDisplayTile(liveDisplayTile: LiveDisplayTile): QSTileImpl<*>
+
+    /** Inject ReadingModeTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(ReadingModeTile.TILE_SPEC)
+    fun bindReadingModeTile(readingModeTile: ReadingModeTile): QSTileImpl<*>
 }
