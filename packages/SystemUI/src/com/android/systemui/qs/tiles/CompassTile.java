baed1fe5157dce0f7f16e01a8d09207787586bbe
Christian Oder <myself5@carbonrom.org>
2019-11-26 23:35:01 +0100

    base: SystemUI: Introduce DataSwitchTile
    * Based on OnePlus' OxygenOS tile, reworked to work with AOSP toggling without requirements on proprietary telephony-ext features
    
    idoybh (YAAP): Adapt to A12, A14
    
    Change-Id: Ie2e280c07f24f9da6b4ee218b72501a2713ce429
    
    -----
    
    DataSwitchTile: Improve the code
    
    * Get rid of deprecated AsyncTask
    * Removed unused imports
    * Finalize global vars
    * Some other formatting and minor stuff
    
    Change-Id: Id3e39c98dac6200301d15bf752f866f47875633d
    
    -----
    
    DataSwitchTile: Add a proper secondary label
    
    just a partial pick from: https://github.com/AOSPA/android_frameworks_base/commit/1cc38c1163c3b48e51016ca3aef8d2f8c7fb3128
    
    Change-Id: I12883ddefcac17777272f53e2dac787b3a9887ac
    
    -----
    
    DataSwitchTile: Redo and fix data switching logic
    
    subIDs aren't necessarily 1 & 2 only, there could actually be multiple per sim index.
    They could be up till 'n' depending on how many operators the user switched.
    
    Fixed described flaws, improved code readability and reliability.
    Sorry to kill the cool use of XOR, it just doesn't fit here.
    
    Test: Insert a new 2nd sim of a new operator, press the tile, observe switched data
    
    Change-Id: If7d0be225283bbc0cfd1f71ab49af8be86c0eaf3
    
    -----
    
    DataSwitchTile: Don't crash when there are no data SIMs
    
    Change-Id: I5c6094650158faf3d426bf7fca8a00cbb79443e2
    
    -----
    
    SystemUI: DataSwitchTile: Always mark as available
    
    Atleast for now...
    A SIM could always be added and hence we can't just assume any static state
    
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Change-Id: I2491343432a888e9c52015cb3871008ab836f88c
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>
    Co-Authored-By: Ido Ben-Hur <idoybh2@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CompassTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CompassTile.java
@@ -174,7 +174,7 @@ public class CompassTile extends QSTileImpl<BooleanState> implements SensorEvent
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.SUPERIOR;
+        return MetricsEvent.CUSTOMIZE;
     }
 
     @Override
