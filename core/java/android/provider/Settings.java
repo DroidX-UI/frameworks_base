bb76204cf8d36405eba05db6419cd997127e3516
Kra1o5 <kra1o5x@gmail.com>
2020-11-09 15:57:18 +0100

    base: Introduce Immersive Navigation [1/3]
    Based in Hide gesture navbar from Syberia project
    
    - base: Disable Immersive Navigation by default
    
    Co-authored-by: DennySPb <dennyspb@gmail.com>
    Co-authored-by: Víctor Santoja <victorsantoja@gmail.com>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I834016808b7fc8e9ef888bf3fb6d70f670dc36f0
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11603,6 +11603,12 @@ public final class Settings {
         public static void setLocationProviderEnabled(ContentResolver cr,
                 String provider, boolean enabled) {
         }
+
+       /**
+         * Immersive Navigation gesture
+         * @hide
+         */
+        public static final String IMMERSIVE_NAVIGATION = "immersive_navigation";
     }
 
     /**
