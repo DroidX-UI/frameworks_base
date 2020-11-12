a599326c4f2322ea65c183bafbbfc679996c8241
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-11-12 14:30:29 +0000

    [SQUASH] Reticker feature (refactored)
    Feature: reticker [1/2]
    
    [someone5678]
    * Based on crDroid Android's cleaned A12 commit[1]
    * Forward port to Android 13
    * Follow the Material You styles
    
    Original commit:
    https://github.com/Descendant-XI/frameworks_base/commit/6ca0cff0da94d07a14ca8695b7bde5eaea9deef5
    
    Ref:
    [1] https://github.com/crdroidandroid/android_frameworks_base/commit/e44ede735e177d8297cfbfa8b22a21041c229ace
    
    Change-Id: I582d0484977f0e850d158e8645171e638fcc0c4c
    Signed-off-by: someone5678 <nemui3353@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    Properly update view for reTicker
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Use Tuner API for reTicker settings
    
    Change-Id: If0e020c0dc822f099ea02c41db73e9bb0be829c7
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    reTicker: Do not show when content/icon is null
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    reTicker: Cleanup
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    reTicker: Do not jump to app when clicked in games
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    SystemUI: Refactor notification reticker
    
    * add interpolators to reveal animation and increase duration
    * reinflate theme on value change
    * add toggle for animation
    * add small reticker style back
    
    original commit message:
    Animation based on iPhone's new Dynamic Island feature.
    
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    SystemUI: Refactor reticker feature
    * Update reticker specs to latest OneUI brief pop up style
    * code rewrite for risingOS
    * fix default reticker background color
    * fix reticker background based on app color exceptions
    * fix visibility race when animating reticker view
    * implement a different reticker animation
    * properly end reticker animation
    * misc code improvements
    
    Change-Id: I11a0dac235f447d777b7ba28e6dba88f797d94e2
    Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5856,6 +5856,18 @@ public final class Settings {
         public static final String CLICK_PARTIAL_SCREENSHOT = "click_partial_screenshot";
 
         /**
+         * reTicker Status
+         * @hide
+         */
+        public static final String RETICKER_STATUS = "reticker_status";
+
+        /**
+         * reTicker Colored
+         * @hide
+         */
+        public static final String RETICKER_COLORED = "reticker_colored";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
