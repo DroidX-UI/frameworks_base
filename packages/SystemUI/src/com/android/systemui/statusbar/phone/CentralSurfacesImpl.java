d9f1a21c034c2fdc968f2e50131c11e558fc4ddd
ezio84 <brabus84@gmail.com>
2020-11-15 09:45:01 +0100

    base: Less boring heads up option [1/2]
    Allow to show heads up only for dialer and messaging
    app when they are not in foreground
    
    Change-Id: I9ce517a5974b1567339bc7d7c84ee35bb89c774b
    
    ---
    
    Less boring headsup: allow any sms/dialer app set as default by user
    thanks GeoZac for the suggestion!
    
    Change-Id: Ie7a725bd0fc579462b736f2e8520d59d14f402f2
    
    -----
    
    base: Less boring heads up option: always show alarm clock headsup
    
    Change-Id: Iea1f6c7a131689eed3d6bee8b28bebd91a56c412
    
    -----
    
    SystemUI: HeadsUp blacklists [1/2]
    
    @xyyx: reworked
    
    Change-Id: I4aa328464406927a9ead708a1efbe37f9ab5d689
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    
    -----
    
    HeadsUp: add timeout option (1/2)
    
    Forward ported to marshmallow/nougat By: @BeansTown106
    this was basically a rewrite as everything about it changed
    
    Change-Id: I9c88d67d25440dff8f545e1330da672be5d36913
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: xyyx <xyyx@mail.ru>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    
    -----
    
    base: HeadsUp snooze function [1/2]
    
    5.1 introduced a snooze feature which activates when swiping up.
    Default snooze time is 1 minute. Let's make it configurable!
    
    Forward ported to marshmallow/Nougat By:@BeansTown106
    *pretty much a rewrite using the logic from my heads up timeout port
    *also added a second toast for 1 minute instead of saying 1 minutes
    included Make Heads Up snooze toast more informative by beanstown106 too
    *tell the user which app is snoozing
    *also center the toast to account for big app names
    Example: SystemUI - Peeking notifications are snoozing for 1 minute.
    
    Change-Id: Ifa4b5abeb669358938c8b5cd0643d3f2caeee436
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: xyyx <xyyx@mail.ru>
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    
    -----
    
    HeadsUpManager: Switch to SysUIToast for showing toasts
    
    Change-Id: Icf5bc46bcd3cbfbb9194995fa7fb0183fa18be40
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    
    -----
    
    HeadsUp: Fix crash while formatting string
    
    - Also fix snooze timer
    
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Change-Id: Ia0c4841bfabdea5138aacfb7a988e8e8dac3d2b9
    
    -----
    
    fixup! base: HeadsUp blacklists/stoplists
    
    Change-Id: Iefbf53bb3437a49f864246e86dbcb5e90310e176
    
    -----
    
    fixup! base: HeadsUp notification time out
    
    Change-Id: I5c5d18926df4a2a654c980cec216c240d11ef149
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: Lars Greiss <kufikugel@googlemail.com>
    Co-authored-by: Lokesh Chamane <lokesh.c703@gmail.com>
    Co-authored-by: SagarMakhar <sagarmakhar@gmail.com>
    Co-authored-by: naveenjohnsonv <naveenjohnsonv@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -46,12 +46,14 @@ import android.app.WallpaperManager;
 import android.app.admin.DevicePolicyManager;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.content.res.Configuration;
+import android.database.ContentObserver;
 import android.graphics.Point;
 import android.hardware.devicestate.DeviceStateManager;
 import android.hardware.fingerprint.FingerprintManager;
@@ -205,6 +207,7 @@ import com.android.systemui.statusbar.notification.NotificationActivityStarter;
 import com.android.systemui.statusbar.notification.NotificationLaunchAnimatorControllerProvider;
 import com.android.systemui.statusbar.notification.NotificationWakeUpCoordinator;
 import com.android.systemui.statusbar.notification.init.NotificationsController;
+import com.android.systemui.statusbar.notification.interruption.NotificationInterruptStateProvider;
 import com.android.systemui.statusbar.notification.interruption.VisualInterruptionDecisionProvider;
 import com.android.systemui.statusbar.notification.row.ExpandableNotificationRow;
 import com.android.systemui.statusbar.notification.row.NotificationGutsManager;
@@ -462,6 +465,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
     private final NotificationGutsManager mGutsManager;
     private final ShadeExpansionStateManager mShadeExpansionStateManager;
     private final KeyguardViewMediator mKeyguardViewMediator;
+    protected final NotificationInterruptStateProvider mNotificationInterruptStateProvider;
     private final VisualInterruptionDecisionProvider mVisualInterruptionDecisionProvider;
     private final BrightnessSliderController.Factory mBrightnessSliderFactory;
     private final FeatureFlags mFeatureFlags;
@@ -529,6 +533,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
     private final DelayableExecutor mMainExecutor;
 
     private int mInteractingWindows;
+    private Handler mMainHandler;
 
     private final ViewMediatorCallback mKeyguardViewMediatorCallback;
     private final ScrimController mScrimController;
@@ -624,6 +629,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
             FalsingCollector falsingCollector,
             BroadcastDispatcher broadcastDispatcher,
             NotificationGutsManager notificationGutsManager,
+            NotificationInterruptStateProvider notificationInterruptStateProvider,
             VisualInterruptionDecisionProvider visualInterruptionDecisionProvider,
             ShadeExpansionStateManager shadeExpansionStateManager,
             KeyguardViewMediator keyguardViewMediator,
@@ -693,6 +699,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
             LockscreenShadeTransitionController lockscreenShadeTransitionController,
             FeatureFlags featureFlags,
             KeyguardUnlockAnimationController keyguardUnlockAnimationController,
+            @Main Handler mainHandler,
             @Main DelayableExecutor delayableExecutor,
             @Main MessageRouter messageRouter,
             WallpaperManager wallpaperManager,
@@ -732,6 +739,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
         mFalsingManager = falsingManager;
         mBroadcastDispatcher = broadcastDispatcher;
         mGutsManager = notificationGutsManager;
+        mNotificationInterruptStateProvider = notificationInterruptStateProvider;
         mVisualInterruptionDecisionProvider = visualInterruptionDecisionProvider;
         mShadeExpansionStateManager = shadeExpansionStateManager;
         mKeyguardViewMediator = keyguardViewMediator;
@@ -799,6 +807,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
         mIsShortcutListSearchEnabled = featureFlags.isEnabled(Flags.SHORTCUT_LIST_SEARCH_LAYOUT);
         mKeyguardUnlockAnimationController = keyguardUnlockAnimationController;
         mMainExecutor = delayableExecutor;
+        mMainHandler = mainHandler;
         mMessageRouter = messageRouter;
         mWallpaperManager = wallpaperManager;
         mJankMonitor = jankMonitor;
@@ -902,6 +911,9 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
         }
 
         createAndAddWindows(result);
+
+        mSbSettingsObserver.observe();
+        mSbSettingsObserver.update();
 
         // Set up the initial notification state. This needs to happen before CommandQueue.disable()
         setUpPresenter();
@@ -2670,6 +2682,16 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
         }
     };
 
+    private void setHeadsUpStoplist() {
+        if (mNotificationInterruptStateProvider != null)
+            mNotificationInterruptStateProvider.setHeadsUpStoplist();
+    }
+
+    private void setHeadsUpBlacklist() {
+        if (mNotificationInterruptStateProvider != null)
+            mNotificationInterruptStateProvider.setHeadsUpBlacklist();
+    }
+
     /**
      * @return true if the screen is currently fully off, i.e. has finished turning off and has
      * since not started turning on.
@@ -2898,6 +2920,41 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
     @Override
     public boolean isDeviceInteractive() {
         return mDeviceInteractive;
+    }
+
+    private SbSettingsObserver mSbSettingsObserver = new SbSettingsObserver(mMainHandler);
+    private class SbSettingsObserver extends ContentObserver {
+        SbSettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.HEADS_UP_STOPLIST_VALUES),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.HEADS_UP_BLACKLIST_VALUES),
+                    false, this, UserHandle.USER_ALL);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            super.onChange(selfChange, uri);
+            if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.HEADS_UP_STOPLIST_VALUES))) {
+                setHeadsUpStoplist();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.HEADS_UP_BLACKLIST_VALUES))) {
+                setHeadsUpBlacklist();
+            }
+            update();
+        }
+
+        public void update() {
+            setHeadsUpStoplist();
+            setHeadsUpBlacklist();
+        }
     }
 
     private final BroadcastReceiver mBannerActionBroadcastReceiver = new BroadcastReceiver() {
