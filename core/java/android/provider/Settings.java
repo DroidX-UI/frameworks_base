89f5d160f5aacda657ca504f4127c57fe57cd32e
jhonboy121 <alfredmathew05@gmail.com>
2021-11-14 13:23:32 +0530

    base: Allow toggling screen off FOD [1/2]
    Change-Id: I1feece846e4dbddb276ad6ed0efac77e5b7986d8
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12128,6 +12128,13 @@ public final class Settings {
         public static final String SYSTEM_BLACK_THEME = "system_black_theme";
 
         /**
+         * Enable udfps detection even when screen is off
+         * Default value is 0
+         * @hide
+         */
+        public static final String SCREEN_OFF_UDFPS_ENABLED = "screen_off_udfps_enabled";
+
+        /**
          * Whether to show privacy indicator for location
          * @hide
          */
