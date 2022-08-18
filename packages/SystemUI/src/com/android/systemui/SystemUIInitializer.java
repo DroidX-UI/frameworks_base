6d9a74f2c472941041ba1fbfb548e4bc7819138e
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-08-18 19:11:14 +0530

    SystemUI: Forward-port smartspace to Android 13
    Change-Id: Iddf5c22562b1858e3159fd939c54890e23b29063

--- packages/SystemUI/src/com/android/systemui/SystemUIInitializer.java
+++ packages/SystemUI/src/com/android/systemui/SystemUIInitializer.java
@@ -138,7 +138,7 @@ public abstract class SystemUIInitializer {
         Dependency dependency = mSysUIComponent.createDependency();
         dependency.start();
 
-        if (mInitializeComponents) {
+        if (initializeComponents) {
             mSysUIComponent.createKeyguardSmartspaceController();
         }
     }
