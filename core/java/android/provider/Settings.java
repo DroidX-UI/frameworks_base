4bbb32b5f140d46441573a25fc10ff267cbdde59
Alberto97 <alberto97@gmail.com>
2022-09-03 09:41:31 +0200

    Implement Auto brightness [1/2]
     * Expose AutoBrightness button
     * Add tunables
     * Use vector drawables for brightness icons
    
    [BadDaemon]
     * Adapt to SystemUI changes
     * Change code style to use lambdas and locals where possible
    
    [marshmello61]
     * Forward port to Android 13
    
    Author: Luca Stefani <luca020400@lineageos.org>
    Date:   Thu Apr 20 18:00:52 2017 +0200
    
        SystemUI: Use AOSP AutoBrightness implementation
    
        Change-Id: I3e3660065407d460f30c6e936d951d4d5eae30c6
    
    Author: Michael Bestas <mikeioannina@gmail.com>
    Date:   Thu Apr 27 14:57:49 2017 +0300
    
        SystemUI: Use already existing overlay to show QS auto brightness
    
        * No need for a new overlay when one exists for years
    
        Change-Id: I9e09a1770f8e645093e880cf1a48597405e0bcf2
    
    Author: Michael Bestas <mkbestas@lineageos.org>
    Date:   Sun Apr 30 13:45:39 2017 +0300
    
        SystemUI: Add tunable for auto brightness icon
    
        Change-Id: I02fe3f5500f95ba9369b952bbec2a073b7ae8316
    
    Author: Alexander Westphal <westphal.alex94@gmail.com>
    Date:   Wed Apr 26 09:20:53 2017 +0100
    
        SystemUI: use vector drawables for brightness icons
    
        Change-Id: I8d22f8de22040d68ed1bfab0da3a78aec717fa62
    
    Author: qjohn <townex22@gmail.com>
    Date:   Sun Jun 18 16:28:10 2017 +0700
    
        SystemUI: Remove unnecessary brightness icons
    
        Change-Id: I7e60ff73125bd0304f9ca14ad5c5a9ce11ffceda
    
    Author: Michael W <baddaemon87@gmail.com>
    Date:   Mon May 25 15:38:33 2020 +0200
    
        SystemUI: Brightness slider: Always show the brightness icon
    
        * Right now, we reserve the space for the icon but it's not displayed
          (reserved space is there so the slider doesn't jump)
        * Always display the icon (if automatic brightness is available)
          so it's more consistent
    
        Change-Id: I5238dc6aad51407349a8d42fae840ea76841dc3d
    
    Author: Michael W <baddaemon87@gmail.com>
    Date:   Fri May 29 20:09:18 2020 +0200
    
        SystemUI: Fix for auto brightness icon being shown when disabled
    
        * With commit 9d42c48c16bba2fcc836cbb68e2979ed9c0d1b23 the case of a
          manually disabled auto brightness icon got lost
        * Re-implement it by also respecting the Settings value instead of just
          the build time flag
    
        Change-Id: I991977be7e1f7c33947073dbd3ce7a800fb158bf
    
    Author: Pranav Vashi <neobuddy89@gmail.com>
    Date:   Sun May 8 23:36:19 2022 +0530
    
        Use brightness icon matching with slider redesign
    
        * Extracted from crdroidandroid/android_frameworks_base@91bd04a
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
        Signed-off-by: Mayur <ultramayur123@gmail.com>
    
    Change-Id: Id743ec835cffe4e732f3625080c629c12c5ea544

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12245,6 +12245,12 @@ public final class Settings {
         public static final String QQS_NUM_COLUMNS_LANDSCAPE = "qqs_num_columns_landscape";
 
         /**
+         * Whether to show the auto brightness icon in quick settings panel.
+         * @hide
+         */
+        public static final String QS_SHOW_AUTO_BRIGHTNESS = "qs_show_auto_brightness";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
