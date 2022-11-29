e6182c8421113e29c3c6a5af3c897f80bb4aeefe
Blaster4385 <venkateshchaturvedi12@gmail.com>
2022-11-29 23:33:19 +0530

    base: Add optional haptic feedback to volume panel [1/2]
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -75,6 +75,8 @@ import android.os.Message;
 import android.os.SystemClock;
 import android.os.Trace;
 import android.os.VibrationEffect;
+import android.os.UserHandle;
+import static android.view.HapticFeedbackConstants.CLOCK_TICK;
 import android.provider.Settings;
 import android.provider.Settings.Global;
 import android.text.InputFilter;
@@ -2507,6 +2509,11 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
                     Events.writeEvent(Events.EVENT_TOUCH_LEVEL_CHANGED, mRow.stream,
                             userLevel);
                 }
+            }
+            final boolean doVibrate = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.VOLUME_PANEL_HAPTIC_FEEDBACK, 1) != 0;
+            if (doVibrate) {
+            seekBar.performHapticFeedback(CLOCK_TICK);
             }
         }
 
