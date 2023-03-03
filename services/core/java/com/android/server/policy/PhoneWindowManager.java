4e0089b8f2d8262c748281a5ae721d16bce65f74
Henrique Silva <jhenrique09.mcz@hotmail.com>
2023-03-03 20:59:59 +0800

    Initial import of lineagehw
    All credits to LineageOS and related authors on:
    
    https://github.com/LineageOS/android_lineage-sdk
    
    Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Change-Id: Ief90ce8c20115d6a1c17c84a9874af6246dc1dcd

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -185,6 +185,7 @@ import android.view.animation.AnimationUtils;
 import android.view.autofill.AutofillManagerInternal;
 
 import com.android.internal.R;
+
 import com.android.internal.accessibility.AccessibilityShortcutController;
 import com.android.internal.accessibility.util.AccessibilityUtils;
 import com.android.internal.app.AssistUtils;
