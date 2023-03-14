5444cadd02aca113167d499297d78b8fd0fcae34
ArianK16a <>
2023-03-14 19:26:33 +0800

    Settings.Secure: Add SCREEN_OFF_UDFPS_ENABLED
    Change-Id: Ic4ee6a21b68a83aeedebbd3205e4ff7b0e1d3034

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11218,7 +11218,15 @@ public final class Settings {
          */
         public static final String EXTRA_AUTOMATIC_POWER_SAVE_MODE =
                 "extra_automatic_power_save_mode";
-
+			
+        /**
+         * Whether UDFPS is active while the screen is off.
+         *
+         * <p>1 if true, 0 or unset otherwise.
+         *
+         * @hide
+         */
+        public static final String SCREEN_OFF_UDFPS_ENABLED = "screen_off_udfps_enabled";
         /**
          * Whether lockscreen weather is enabled.
          *
