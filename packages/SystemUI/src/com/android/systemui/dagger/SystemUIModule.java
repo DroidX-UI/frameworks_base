df8ca68a58477480813d23e6656ee9631ac559ef
jhenrique09 <jhsv09@gmail.com>
2023-03-22 14:27:30 +0000

    SystemUI: Update google smartspace to 13-QPR2
    * Updated from https://github.com/PixelExperience/vendor_pixel-framework/tree/2e0803d66857a599ee340656ed36140bc47dc668
    
    Co-authored-by: basamaryan <basam.aryan@gmail.com>
    Change-Id: I009c3d72f5a9e657d25c5c2bcb6544d0446f6b7d

--- packages/SystemUI/src/com/android/systemui/dagger/SystemUIModule.java
+++ packages/SystemUI/src/com/android/systemui/dagger/SystemUIModule.java
@@ -65,6 +65,7 @@ import com.android.systemui.recents.Recents;
 import com.android.systemui.screenshot.dagger.ScreenshotModule;
 import com.android.systemui.security.data.repository.SecurityRepositoryModule;
 import com.android.systemui.settings.dagger.MultiUserUtilsModule;
+import com.android.systemui.settings.UserTracker;
 import com.android.systemui.shade.ShadeController;
 import com.android.systemui.smartspace.dagger.SmartspaceModule;
 import com.android.systemui.statusbar.CommandQueue;
@@ -307,8 +308,8 @@ public abstract class SystemUIModule {
     @Provides
     @SysUISingleton
     static KeyguardMediaViewController provideKeyguardMediaViewController(Context context, BcSmartspaceDataPlugin bcSmartspaceDataPlugin,
-            @Main DelayableExecutor delayableExecutor, NotificationMediaManager notificationMediaManager, BroadcastDispatcher broadcastDispatcher) {
-        return new KeyguardMediaViewController(context, bcSmartspaceDataPlugin, delayableExecutor, notificationMediaManager, broadcastDispatcher);
+            @Main DelayableExecutor delayableExecutor, NotificationMediaManager notificationMediaManager, BroadcastDispatcher broadcastDispatcher, UserTracker userTracker, Handler mainHandler) {
+        return new KeyguardMediaViewController(context, bcSmartspaceDataPlugin, delayableExecutor, notificationMediaManager, broadcastDispatcher, userTracker, mainHandler);
     }
 
     @Provides
--- packages/SystemUI/src/com/google/android/systemui/smartspace/KeyguardMediaViewController.java
+++ packages/SystemUI/src/com/google/android/systemui/smartspace/KeyguardMediaViewController.java
@@ -6,6 +6,8 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.media.MediaMetadata;
 import android.os.UserHandle;
+import android.os.Handler;
+import android.os.HandlerExecutor;
 import android.text.TextUtils;
 import android.view.View;
 
@@ -13,7 +15,7 @@ import com.android.systemui.R;
 import com.android.systemui.broadcast.BroadcastDispatcher;
 import com.android.systemui.dagger.SysUISingleton;
 import com.android.systemui.plugins.BcSmartspaceDataPlugin;
-import com.android.systemui.settings.CurrentUserTracker;
+import com.android.systemui.settings.UserTracker;
 import com.android.systemui.statusbar.NotificationMediaManager;
 import com.android.systemui.util.concurrency.DelayableExecutor;
 
@@ -51,7 +53,19 @@ public final class KeyguardMediaViewController {
     private BcSmartspaceDataPlugin.SmartspaceView smartspaceView;
     private CharSequence title;
     private final DelayableExecutor uiExecutor;
-    private CurrentUserTracker userTracker;
+
+    private final Handler mMainHandler;
+    private final UserTracker mUserTracker;
+
+    private final UserTracker.Callback mUserChangedCallback =
+            new UserTracker.Callback() {
+                @Override
+                public void onUserChanged(int newUser, Context userContext) {
+                   if (userContext != null) {
+                    reset();
+                   }
+                }
+            };
 
     @Inject
     public KeyguardMediaViewController(
@@ -59,17 +73,23 @@ public final class KeyguardMediaViewController {
             BcSmartspaceDataPlugin plugin,
             DelayableExecutor uiExecutor,
             NotificationMediaManager mediaManager,
-            BroadcastDispatcher broadcastDispatcher) {
+            BroadcastDispatcher broadcastDispatcher,
+            UserTracker userTracker,
+            Handler mainHandler) {
         Intrinsics.checkNotNullParameter(context, "context");
         Intrinsics.checkNotNullParameter(plugin, "plugin");
         Intrinsics.checkNotNullParameter(uiExecutor, "uiExecutor");
         Intrinsics.checkNotNullParameter(mediaManager, "mediaManager");
         Intrinsics.checkNotNullParameter(broadcastDispatcher, "broadcastDispatcher");
+        Intrinsics.checkNotNullParameter(userTracker, "userTracker");
+        Intrinsics.checkNotNullParameter(mainHandler, "mainHandler");
         this.context = context;
         this.plugin = plugin;
         this.uiExecutor = uiExecutor;
         this.mediaManager = mediaManager;
         this.broadcastDispatcher = broadcastDispatcher;
+        this.mUserTracker = userTracker;
+        this.mMainHandler = mainHandler;
         mediaComponent = new ComponentName(context, KeyguardMediaViewController.class);
     }
 
@@ -110,13 +130,7 @@ public final class KeyguardMediaViewController {
                         notificationMediaManager.removeCallback(keyguardMediaViewController);
                     }
                 });
-        userTracker =
-                new CurrentUserTracker(broadcastDispatcher) {
-                    @Override
-                    public void onUserSwitched(int i) {
-                        reset();
-                    }
-                };
+        mUserTracker.addCallback(mUserChangedCallback, new HandlerExecutor(mMainHandler));
     }
 
     public final void updateMediaInfo(MediaMetadata mediaMetadata, int i) {
@@ -149,16 +163,11 @@ public final class KeyguardMediaViewController {
                             .setSubtitle(artist)
                             .setIcon(mediaManager.getMediaIcon())
                             .build();
-            CurrentUserTracker currentUserTracker = userTracker;
-            if (currentUserTracker == null) {
-                Intrinsics.throwUninitializedPropertyAccessException("userTracker");
-                throw null;
-            }
             SmartspaceTarget build2 =
                     new SmartspaceTarget.Builder(
                                     "deviceMedia",
                                     mediaComponent,
-                                    UserHandle.of(currentUserTracker.getCurrentUserId()))
+                                    UserHandle.of(mUserTracker.getUserId()))
                             .setFeatureType(41)
                             .setHeaderAction(build)
                             .build();
