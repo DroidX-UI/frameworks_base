b3215537dbe84231af91682f0633db3187d1154c
jhenrique09 <jhsv09@gmail.com>
2023-03-24 12:05:26 -0300

    fixup - PixelPropsUtils: Spoof user build
    Change-Id: I3f08ca3c90448251e7f1b7a20e8e4b942bcd2d5c

--- core/java/com/android/internal/util/droidx/PixelPropsUtils.java
+++ core/java/com/android/internal/util/droidx/PixelPropsUtils.java
@@ -233,6 +233,8 @@ public class PixelPropsUtils {
     }
 
     public static void setProps(String packageName) {
+        propsToChangeGeneric.forEach((k, v) -> setPropValue(k, v));
+
         if (packageName == null || packageName.isEmpty()) {
             return;
         }
@@ -246,7 +248,6 @@ public class PixelPropsUtils {
                 || Arrays.asList(extraPackagesToChange).contains(packageName)) {
 
             Map<String, Object> propsToChange = new HashMap<>();
-            propsToChange.putAll(propsToChangeGeneric);
 
             boolean isPixelDevice = Arrays.asList(pixelCodenames).contains(SystemProperties.get(DEVICE));
 
