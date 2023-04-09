e4c4d314bfb91ca751638c9e41ba7f4cd2988636
Tim Zimmermann <tim@linux4.de>
2023-04-09 20:30:05 +0200

    SystemUI: Add settings to change QS column count [1/2]
    * Replace max tiles with max columns for round qs
      in order to properly animate qqs => qs transition
    
    Change-Id: Id77c0ee01d8a5d7ca3ac6d4167c1cf30d450bb4d

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -17,6 +17,7 @@
 package com.android.systemui.qs;
 
 import static com.android.systemui.util.Utils.useQsMediaPlayer;
+import static com.android.systemui.util.qs.QSStyleUtils.isRoundQS;
 
 import android.annotation.NonNull;
 import android.annotation.Nullable;
@@ -34,6 +35,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.accessibility.AccessibilityNodeInfo;
 import android.widget.LinearLayout;
+import android.provider.Settings;
 
 import androidx.annotation.VisibleForTesting;
 
@@ -125,6 +127,13 @@ public class QSPanel extends LinearLayout implements Tunable {
         mMaxColumnsPortrait = getResources().getInteger(R.integer.qs_panel_num_columns);
         mMaxColumnsLandscape = getResources().getInteger(R.integer.qs_panel_num_columns_landscape);
         mMaxColumnsMediaPlayer = getResources().getInteger(R.integer.qs_panel_num_columns_media);
+        if (isRoundQS()) {
+            mMaxColumnsPortrait = Settings.Secure.getInt(context.getContentResolver(),
+                    Settings.Secure.QS_NUM_COLUMNS, mMaxColumnsPortrait);
+            mMaxColumnsLandscape = Settings.Secure.getInt(context.getContentResolver(),
+                    Settings.Secure.QS_NUM_COLUMNS_LANDSCAPE, mMaxColumnsLandscape);
+        }
+
         mContext = context;
 
         setOrientation(VERTICAL);
@@ -748,6 +757,12 @@ public class QSPanel extends LinearLayout implements Tunable {
         default boolean setMaxColumns(int maxColumns) {
             return false;
         }
+
+        /** Gets the max number of columns to show
+         *
+         * @return The maximum number of visible columns.
+         */
+        int getMaxColumns();
 
         /**
          * Sets the expansion value and proposedTranslation to panel.
--- packages/SystemUI/src/com/android/systemui/qs/QuickQSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickQSPanel.java
@@ -16,8 +16,11 @@
 
 package com.android.systemui.qs;
 
+import static com.android.systemui.util.qs.QSStyleUtils.isRoundQS;
+
 import android.content.Context;
 import android.content.res.Configuration;
+import android.provider.Settings;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.accessibility.AccessibilityNodeInfo;
@@ -52,6 +55,15 @@ public class QuickQSPanel extends QSPanel {
         mMaxColumnsPortrait = getResources().getInteger(R.integer.quick_qs_panel_num_columns);
         mMaxColumnsLandscape = getResources().getInteger(R.integer.quick_qs_panel_num_columns_landscape);
         mMaxColumnsMediaPlayer = getResources().getInteger(R.integer.quick_qs_panel_num_columns_media);
+        if (isRoundQS()) {
+            mMaxColumnsPortrait = Settings.Secure.getInt(context.getContentResolver(),
+                    Settings.Secure.QQS_NUM_COLUMNS, mMaxColumnsPortrait);
+            mMaxColumnsLandscape = Settings.Secure.getInt(context.getContentResolver(),
+                    Settings.Secure.QQS_NUM_COLUMNS_LANDSCAPE, mMaxColumnsLandscape);
+
+            mMaxTiles = Math.max(mMaxColumnsPortrait, mMaxColumnsLandscape);
+            mMaxTiles = Math.max(mMaxColumnsMediaPlayer, mMaxTiles);
+        }
     }
 
     @Override
