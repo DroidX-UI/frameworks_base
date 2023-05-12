283143108b5ffc494d8ef7b66633dfc9497817ea
minaripenguin <minaripenguin@users.noreply.github.com>
2023-05-12 23:44:51 +0800

    SystemUI: Hide notification icon area if headsup is visible
    Change-Id: I2dc5a72015f3aee925311156350f21ad4ee60961
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
@@ -514,6 +514,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
             if (!isRightClock) {
                 state |= DISABLE_CLOCK;
             }
+            state |= DISABLE_NOTIFICATION_ICONS;
         }
 
         if (mOngoingCallController.hasOngoingCall()) {
