0c857252ef9a07cd86279d2c75df5a7537564bab
p4042 f <adrianlam230@gmail.com>
2023-08-03 11:59:18 +0000

    Revert "Feature: reticker [1/2]"
    This reverts commit 6382dd71b1250848b9f2d01327a21b4659e6c390.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5801,17 +5801,6 @@ public final class Settings {
          */
         public static final String DOUBLE_TAP_SLEEP_LOCKSCREEN = "double_tap_sleep_lockscreen";
 
-        /**
-         * reTicker Status
-         * @hide
-         */
-        public static final String RETICKER_STATUS = "reticker_status";
-
-        /**
-         * reTicker Colored
-         * @hide
-         */
-        public static final String RETICKER_COLORED = "reticker_colored";
         
         /** Whether to show ambient or lockscreen if AoD is disabled
          * and we do a wake gesture like lift to wake or double tap
