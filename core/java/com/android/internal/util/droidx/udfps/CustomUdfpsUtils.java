81172e884d320c60e0b039b3c3bb92eedf2c211c
Joey Huab <joey@evolution-x.org>
2023-10-24 02:18:49 +0900

    base: Rename UdfpsUtils -> CustomUdfpsUtils
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- core/java/com/android/internal/util/droidx/udfps/CustomUdfpsUtils.java
+++ core/java/com/android/internal/util/droidx/udfps/CustomUdfpsUtils.java
@@ -13,13 +13,13 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-package com.android.internal.util.euclid.udfps;
+package com.android.internal.util.droidx.udfps;
 
 import android.content.Context;
 
 import com.android.internal.util.ArrayUtils;
 
-public class UdfpsUtils {
+public class CustomUdfpsUtils {
     public static boolean hasUdfpsSupport(Context context) {
         int[] udfpsProps = context.getResources().getIntArray(
                 com.android.internal.R.array.config_udfps_sensor_props);
