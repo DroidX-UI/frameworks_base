1312e78f64d461ba9845cb50e8601e13c3ce5e4c
someone5678 <someone5678@users.noreply.github.com>
2024-03-22 12:38:38 +0900

    fixup! base: Add api to take screenshots
    * Remove partial screenshot related code from utils and Gesture Nav
    
    Change-Id: I3509b38cbc4190f935227fe4b7c99899ab385712
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -217,7 +217,7 @@ import com.android.internal.policy.LogDecelerateInterpolator;
 import com.android.internal.policy.PhoneWindow;
 import com.android.internal.policy.TransitionAnimation;
 import com.android.internal.statusbar.IStatusBarService;
-import com.android.internal.util.blaze.BlazeUtils;
+import com.android.internal.util.droidx.DroidXUtils;
 import com.android.internal.widget.LockPatternUtils;
 import com.android.server.AccessibilityManagerInternal;
 import com.android.server.ExtconStateObserver;
@@ -6452,10 +6452,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     public void sendCustomAction(Intent intent) {
         String action = intent.getAction();
         if (action != null) {
-            if (BlazeUtils.INTENT_SCREENSHOT.equals(action)) {
-                interceptScreenshotChord(TAKE_SCREENSHOT_FULLSCREEN, SCREENSHOT_KEY_OTHER, 0);
-            } else if (BlazeUtils.INTENT_REGION_SCREENSHOT.equals(action)) {
-                interceptScreenshotChord(TAKE_SCREENSHOT_SELECTED_REGION, SCREENSHOT_KEY_OTHER, 0);
+            if (DroidXUtils.INTENT_SCREENSHOT.equals(action)) {
+                interceptScreenshotChord(SCREENSHOT_KEY_OTHER, 0);
             }
         }
     }
