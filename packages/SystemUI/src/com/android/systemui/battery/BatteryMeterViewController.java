d41757dc16778fccf3b9e7a0b804d96e2b886aef
LuK1337 <priv.luk@gmail.com>
2024-08-01 19:11:18 +0200

    fixup! BatteryMeterView: Hide icon on battery-less devices
    Change-Id: I66eab030f69412def09a4e324f9b3bda74736b6d

--- packages/SystemUI/src/com/android/systemui/battery/BatteryMeterViewController.java
+++ packages/SystemUI/src/com/android/systemui/battery/BatteryMeterViewController.java
@@ -127,6 +127,13 @@ public class BatteryMeterViewController extends ViewController<BatteryMeterView>
                 }
 
                 @Override
+                public void onBatteryPresentChanged(boolean batteryPresent) {
+                    mView.setBatteryPresence(batteryPresent);
+                    mView.setVisibility(!batteryPresent || batteryStyle
+                            ? View.GONE : View.VISIBLE);
+                }
+
+                @Override
                 public void dump(@NonNull PrintWriter pw, @NonNull String[] args) {
                     pw.print(super.toString());
                     pw.println(" location=" + mLocation);
@@ -159,12 +166,8 @@ public class BatteryMeterViewController extends ViewController<BatteryMeterView>
         mBatteryController = batteryController;
 
         mView.setBatteryEstimateFetcher(mBatteryController::getEstimatedTimeRemainingString);
-        mView.setBatteryPresence(mBatteryController.isPresent());
         mView.setDisplayShieldEnabled(
                 getContext().getResources().getBoolean(R.bool.flag_battery_shield_icon));
-        if (!mBatteryController.isPresent()) {
-            mView.setVisibility(View.GONE);
-        }
 
         mSettingObserver = new SettingObserver(mMainHandler);
     }
