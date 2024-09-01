812e76b0fba6c870794924722a2ae3bfe92697e8
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-09-01 09:46:29 +0530

    PropImitationHooks: Make it a hidden API
    Change-Id: I271bc644444581565847d56766ca85bc7d272436
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -39,6 +39,9 @@ import java.util.Arrays;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * @hide
+ */
 public class PropImitationHooks {
 
     private static final String TAG = "PropImitationHooks";
